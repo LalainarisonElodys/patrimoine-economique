a752fe87448b0ea9608669d98c216b0b2d654e7f
LALAINARISON Elodys <hei.elodys@gmail.com>
2024-08-29 11:25:21 +0300

    feat: add header design

--- ui/src/component/Header.jsx
+++ ui/src/component/Header.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header className='bg-black fixed-top'>
-          <span className='text-warning'>Gestion de patrimoine</span>
-            <nav>
-                <ul>
-                    <li ><Link to="/patrimoine">Patrimoine</Link></li>
-                    <li><Link to="/possession">Possessions</Link></li>
-                </ul>
-            </nav>
-        </header>
+        <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    <span className="text-warning">Gestion de Patrimoine</span>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link as={Link} to="/patrimoine">Patrimoine</Nav.Link>
+                        <Nav.Link as={Link} to="/possession">Possessions</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
